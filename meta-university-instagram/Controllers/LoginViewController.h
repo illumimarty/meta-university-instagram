de008f65dac15465a399e8a804f78e7f5a6dd817
Marty Nodado <mdrnodado@gmail.com>
2022-05-30 18:38:50 -0700

    initialized view controllers in storyboard

---
+++ meta-university-instagram/Controllers/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  meta-university-instagram
+//
+//  Created by Marthan Nodado on 5/30/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END

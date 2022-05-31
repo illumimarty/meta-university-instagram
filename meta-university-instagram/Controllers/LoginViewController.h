268cac061f6c9322c4f07094ed86600f09d6be2f
Marty Nodado <mdrnodado@gmail.com>
2022-05-30 20:08:10 -0700

    implemented register user feature

--- meta-university-instagram/Controllers/LoginViewController.h
+++ meta-university-instagram/Controllers/LoginViewController.h
@@ -17,7 +17,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (IBAction)didTapLogin:(id)sender;
 - (IBAction)didTapRegister:(id)sender;
-
+- (void)registerUser;
 
 @end
 
--- meta-university-instagram/Controllers/LoginViewController.m
+++ meta-university-instagram/Controllers/LoginViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "LoginViewController.h"
+#import "Parse/Parse.h"
 
 @interface LoginViewController ()
 
@@ -29,8 +30,30 @@
 */
 
 - (IBAction)didTapRegister:(id)sender {
+    [self registerUser];
 }
 
 - (IBAction)didTapLogin:(id)sender {
+}
+
+- (void)registerUser {
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameTextField.text;
+    newUser.password = self.passwordTextField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+            [self performSegueWithIdentifier:@"loginToHome" sender:nil];
+        }
+    }];
 }
 @end

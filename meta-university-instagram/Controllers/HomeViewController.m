2c80e2a6a817e83442e501f628c531fdff74af6f
Marty Nodado <mdrnodado@gmail.com>
2022-06-01 14:21:09 -0700

    implemented fetching scheme

--- meta-university-instagram/Controllers/HomeViewController.m
+++ meta-university-instagram/Controllers/HomeViewController.m
@@ -6,6 +6,8 @@
 //
 
 #import "HomeViewController.h"
+#include <Parse/Parse.h>
+#include "Post.h"
 
 @interface HomeViewController ()
 
@@ -16,6 +18,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self fetchPosts];
+}
+
+- (void)fetchPosts {
+    PFQuery *postQuery = [Post query];
+    [postQuery orderByDescending:@"createdAt"];
+    [postQuery includeKey:@"author"];
+    postQuery.limit = 20;
+
+    // fetch data asynchronously
+    [postQuery findObjectsInBackgroundWithBlock:^(NSArray<Post *> * _Nullable posts, NSError * _Nullable error) {
+        if (posts) {
+            NSLog(@"%@", posts);
+            // do something with the data fetched
+        }
+        else {
+            // handle error
+        }
+    }];
 }
 
 /*
